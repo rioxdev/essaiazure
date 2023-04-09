dfb01aa88fa2e0fc60664298a3c32d433360b864
Henrio Randriasoa <rioxdev@gmail.com>
2023-04-09 18:53:15 +0200

    added Date controller

---
+++ EssaiAzure.Api/Controllers/DateController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EssaiAzure.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DateController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Today()
+        {
+            return DateTime.Now.ToLongDateString();
+        }
+
+    }
+}
